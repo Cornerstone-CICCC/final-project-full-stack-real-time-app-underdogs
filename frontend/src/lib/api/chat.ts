5717c461693cd5e6e18b5b0724ad2133062fc469
yk-graph <yokokura.tatsuya@gmail.com>
2026-06-16 20:39:19 -0700

    feat: POST chat room

--- frontend/src/lib/api/chat.ts
+++ frontend/src/lib/api/chat.ts
@@ -1,9 +1,30 @@
 import { apiFetch } from "@lib/api/client"
 
-export async function createChat(visitorId: string): Promise<{ id: string }> {
+type CreateChatBody = {
+  visitorId: string
+  firstName: string
+  lastName: string
+}
+
+type SencMessageBody = {
+  from: string
+  text: string
+}
+
+export async function createChat({
+  visitorId,
+  firstName,
+  lastName,
+}: CreateChatBody): Promise<{ chatId: string }> {
   return await apiFetch("/chats", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ visitorId }),
+    body: JSON.stringify({
+      visitorId,
+      firstName,
+      lastName,
+    }),
   })
 }
+
+export async function sendMessage({ from, text }: SencMessageBody) {}
