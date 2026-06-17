4356ad73fe2efcaf8c796be246d14f434d914849
yk-graph <yokokura.tatsuya@gmail.com>
2026-06-16 18:11:21 -0700

    feat: modal UI

---
+++ frontend/src/stores/chatModal.ts
@@ -0,0 +1,5 @@
+import { atom } from 'nanostores';
+
+export const isModalOpen = atom<boolean>(false);
+export const chatModalStatus = atom<'register' | 'chat'>('register');
+export const currentChatId = atom<string | null>(null)
